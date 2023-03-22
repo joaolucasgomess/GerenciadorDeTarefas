3c6afce6307152291e136850384d815aa3cc6ab6
JOAO LUCAS GOMES NAPOLES <1-22-11755@aluno.faminas.edu.br>
2023-03-21 21:08:14 -0300

    novas alterações no metodo de atualizar o arquivo

--- GerenciadorDeArquivos.java
+++ GerenciadorDeArquivos.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class GerenciadorDeArquivos{
    
@@ -27,27 +28,57 @@ public class GerenciadorDeArquivos{
       }
    }
    
-   public static void atualizarArquivo(Tarefa tarefa, String nomeDoArquivo){
+   /*public static void atualizarArquivo(Tarefa tarefa, String nomeDoArquivo){
       try{
          FileReader arquivo = new FileReader(nomeDoArquivo);
          BufferedReader dadosDoArquivo = new BufferedReader(arquivo);
          
          String linha = dadosDoArquivo.readLine();
+         int numeroLinha = 1;
          
          while(linha != null){
             if(linha.contains(tarefa.getTitulo())){
-               String[] dadosDaLinha = linha.split(";");
-               dadosDaLinha[3] = tarefa.getDataConclusa().toString();
-               dadosDaLinha[4] = tarefa.getStatus().toString();
-               adicionarAoArquivo(tarefa, nomeDoArquivo);
-               //TODO
+               atualizarLinhaEscolhida(tarefa, nomeDoArquivo, numeroLinha);
             }
+            numeroLinha++;
          }
          dadosDoArquivo.close();
       }catch(IOException e){
          System.out.println("deu erro mano");
       }
       
+   }*/
+   
+   public static void atualizarArquivo(Tarefa tarefa, String nomeDoArquivo){
+      try {
+         // Abrir o arquivo em modo leitura e escrita
+         RandomAccessFile arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
+    
+         // Localizar a posição da linha que deseja sobrescrever
+         //String linha = arquivo.readLine();
+         
+         while((arquivo.readLine()) != null){
+            if((arquivo.readLine()).contains(tarefa.getTitulo())){
+               String novaLinha =  tarefa.getTitulo() + ";" + tarefa.getDescricao() + ";" + tarefa.getDataCriacao() + ";" + tarefa.getDataConclusao() + ";" + tarefa.getStatus() + ";" + tarefa.getIdTarefa();
+               arquivo.writeBytes(novaLinha);
+               arquivo.close();
+            }
+         }
+         
+         /*for (int i = 1; i < numeroLinha; i++) {
+            arquivo.readLine();
+         }
+         long posicaoInicial = arquivo.getFilePointer();
+    
+         // Sobrescrever a linha
+         String novaLinha = "Nova linha que irá sobrescrever a linha antiga";
+         arquivo.writeBytes(novaLinha);*/
+    
+         // Fechar o arquivo
+         arquivo.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+      }
    }
    
    /*public static void verificarSeArquivoExiste(File ){
@@ -57,39 +88,14 @@ public class GerenciadorDeArquivos{
    /*public static void criarArquivo(){
       
    }*/
-   
-   /*try {
-    // Abrir o arquivo em modo leitura e escrita
-    RandomAccessFile arquivo = new RandomAccessFile("caminho/do/arquivo.txt", "rw");
-    
-    // Localizar a posição da linha que deseja sobrescrever
-    int numeroDaLinha = 3; // exemplo
-    arquivo.seek(0);
-    for (int i = 1; i < numeroDaLinha; i++) {
-        arquivo.readLine();
-    }
-    long posicaoInicial = arquivo.getFilePointer();
-    
-    // Sobrescrever a linha
-    String novaLinha = "Nova linha que irá sobrescrever a linha antiga";
-    arquivo.writeBytes(novaLinha);
-    
-    // Fechar o arquivo
-    arquivo.close();
-} catch (IOException e) {
-    e.printStackTrace();
-}
-Sim, é possível sobrescrever uma linha em um arquivo sem reescrever o arquivo inteiro. Para isso, você precisa seguir os seguintes passos:
+ 
+
+/*Sim, é possível sobrescrever uma linha em um arquivo sem reescrever o arquivo inteiro. Para isso, você precisa seguir os seguintes passos:
 
 Abrir o arquivo em modo leitura e escrita (RandomAccessFile).
 Localizar a posição da linha que deseja sobrescrever usando o método seek().
 Sobrescrever a linha usando o método writeBytes() ou writeUTF() do objeto RandomAccessFile.
 
-Neste exemplo, a linha que está na posição 3 do arquivo será sobrescrita pela nova linha. Note que é importante abrir o arquivo em modo leitura e escrita ("rw"), caso contrário não será possível realizar a sobrescrita.
+Neste exemplo, a linha que está na posição 3 do arquivo será sobrescrita pela nova linha. Note que é importante abrir o arquivo em modo leitura e escrita ("rw"), caso contrário não será possível realizar a sobrescrita.*/
 
-
-
-
-
-*/
 }
